10ece0e7ef773dd5f533d5e507ac1040b9922d6b
Michael Kupchick <michaelkupchick@michaels-imac.local>
2016-02-09 18:27:37 +0200

    Initial Commit

---
+++ Matchismo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Matchismo
+//
+//  Created by Michael Kupchick on 2/9/16.
+//  Copyright © 2016 Michael Kupchick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Matchismo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Matchismo
+//
+//  Created by Michael Kupchick on 2/9/16.
+//  Copyright © 2016 Michael Kupchick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
